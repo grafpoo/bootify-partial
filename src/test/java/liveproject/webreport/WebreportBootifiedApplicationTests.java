6243e0d45ca254df0551f59194b1c48a6cd08561
john <grafpoo@gmail.com>
2021-03-22 07:25:53 -0400

    partial

---
+++ src/test/java/liveproject/webreport/WebreportBootifiedApplicationTests.java
@@ -0,0 +1,14 @@
+package liveproject.webreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+class WebreportBootifiedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
